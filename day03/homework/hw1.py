35d7a4be3638f702ab91b00c4777ba30918b5c87
gega76 <ggabunia571@gmail.com>
2025-07-09 13:36:07 +0400

    1

---
+++ day03/homework/hw1.py
@@ -0,0 +1,73 @@
+#დავალება 1
+
+num_1=20
+num_2=40
+num_3=num_1+num_2
+num_4=num_2-num_1
+num_5=num_2/num_1
+num_6=num_2*num_1
+
+
+
+
+name="gega"
+name="ciala"
+name="mayvala"
+name="zura"
+name="aleqsi"
+name="nikolozi"
+
+print(name)
+
+
+
+number=20
+Number=80
+nUmber=70
+nuMber=9000
+numBer=350
+
+print(number)
+print(Number)
+print(nUmber)
+print(nuMber)
+print(numBer)
+
+
+
+name="giorgi" #არ შეიძლება ცვლადეფის რიცხვით დაწყება
+user_name= "bubunauri" #როდესაც snake_case ვიყენებთ აუცილებელია ორი სიტყვის შუაში გამოვვყოთ ქვედა ტირე
+user_name= "goga"  #string ტიპის მონაცემს ყოველთვის სჭირდება ბრჭყალები
+user_name= "axalaia" 
+
+
+
+
+nick_name= "mayvalam"
+nick_name1=  "manqanis"
+nick_name2= "tareba"
+nick_name3=  "ar"
+nick_name4=  "icis"
+
+print(nick_name + nick_name1 + nick_name2 + nick_name3 + nick_name4)
+#string-ების შეერებას კონკატინაცია ეწოდება
+
+
+my_name= "gega"
+number1= 5
+
+print(my_name)
+print(my_name)
+print(my_name)
+print(my_name)
+print(my_name)
+print(my_name)
+print(my_name)
+print(my_name)
+print(my_name)
+print(my_name)
+
+#print(my_name + number1) 
+#print(my_name - number1)     #ამ სამი მოქმედების განხორციელება არ შეიძლება
+#print(number1 / my_name)
+print(my_name * number1)
