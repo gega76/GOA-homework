bbb9c95340cdad7965c41f4389090be4e74f6d9b
gega76 <ggabunia571@gmail.com>
2025-07-17 02:18:18 -0700

    1

---
+++ day05/classwork/cw1.py
@@ -0,0 +1,8 @@
+h=int(input("choose anu number"))
+s=int(input("choose second number"))
+
+print(h<s)
+print(h>s)
+print(h==s)
+
+
---
+++ day05/homework/hw1.py
@@ -0,0 +1,74 @@
+#დავალება 1
+
+#ჩვენ ვისწავლეთ 3 ოპერატორი ესენია == , < , > 
+
+#დავალება 2
+
+h=int(input("choose any number"))
+v=int(input("choose second nubmer"))
+
+print(h==v)
+print(h<v)
+print(h>v)
+
+#დავალება 3
+
+name = input("choose any name")
+name1 = input("choose second name")
+name2 = input("choose third name")
+name3 = input("choose forth name")
+name4 = input("choose fifth name")
+
+print(name+name1+name2+name3+name4)
+
+
+#დავალება 4
+
+j=int(input("choose any number"))
+k=int(input("choose second number"))
+i=int(input("choose third number"))
+c=int(input("choose last number"))
+
+print((j+k+i+c) / 4)
+
+#დავალება 5
+
+nick_name = "nugzari"
+height = 1.82
+num1 = 82
+boolean = (80>40)
+
+print(type(nick_name))
+print(type(height))
+print(type(num1))
+print(type(boolean))
+
+
+#დავალება 6
+
+last_name = "gabunia"
+car_brand = "BMW"
+
+print(last_name = car_brand)
+
+# დავალება 7
+
+
+number1 =int("42")
+number2 =int("63")
+number3 =int("90")
+number4 =int("53")
+
+
+print(number1 + number2 + number3 + number4)
+
+
+#დავალება 8
+
+
+random_num1=str(43)
+random_num2=str(23)
+random_num3=str(54)
+
+
+print(random_num1+random_num2+random_num3)
