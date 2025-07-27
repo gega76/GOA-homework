0c45fe6710b59237e641525982c58898ed587949
gega76 <ggabunia571@gmail.com>
2025-07-27 01:14:29 -0700

    1

---
+++ day08/classwork/cw.py
@@ -0,0 +1,13 @@
+print(20==40 and 48>21)
+print(78>80 and 67==67)
+print(54>53 and 21>20)
+
+
+print(20==20 or 70<20)
+print(74>73 or 90>21)
+print(12==12 or 2132<11111)
+
+
+
+for i in range(20):
+    print("gega gabunia")
---
+++ day08/homework/hw.py
@@ -0,0 +1,102 @@
+#დავალება 1 
+print(20==20 and 40<39)
+print(88>12 and 21>54)
+print(20==21 and 43>21)
+print(33>34 and 99==200)
+print(76>32 and 82==83)
+
+print(78>23 or 67==67)
+print(32==32 or 78==90)
+print(54>12 or 61<50)
+print(22==90 or 57>11)
+print(20==20 or 0==0)
+
+#დავალება 2
+
+#sequencing ----> ეს არის თანმიმდევრობა ანუ ჩვენი კოდი იკითხება ზევიდან ქვემოთ
+
+#interation ----> ეს არის გამეორება ანუ ერთი და იგივე int or str or float მეორდება ბევჯერ
+
+#selection ----> არჩევა
+
+
+#დავალება 3
+
+name = "gega"
+name = "jora"
+
+print(name) #ეს მოქმედება არის sequencing რადგან ჩვენი კოდი იკითხევა ზევიდან ქვევით ხოლო სახელი gega შემდეგ name განახლდა და გახდა jora
+
+
+#დავალება 4
+
+
+
+#loop ---- ეს არის გამეორება და ჩვენ ის გვეხმარება რომ ერთი და იგივე რამ არ დავწეროთ 10-ჯერ 100-ჯერ 
+
+
+
+#დავალება 5
+
+
+
+#ფუნქციას range()  გადაეცემა თუ რამდეჯერ გამეორდება ჩვენს მიერ დაწერილი რიცხვი ან სახელი მაგ:for i in range()
+    #print(gega)
+
+#დავალება 6
+
+for i in range(1):
+    print("mersedes")
+
+
+#დავალება 7
+
+for i in range(100):
+    print("gabunia")
+
+
+#დავალება 8
+
+for i in range(46):
+    print("mwvane")
+
+
+
+#დავალება 9 
+
+for i in range(32):
+    print("g")
+
+
+#დავალება 10
+
+
+h=input("choose any name")
+s=input("choose another name")
+y=input("choose third name")
+g=input("choose a number")
+
+
+print(h+s+y+g)
+
+
+#დავალება 11
+
+last_name= "gabunia"
+age = 54
+height = 1.82
+boolean = ( 23==23 or 92<91)
+
+print(type(last_name))
+print(type(age))
+print(type(height))
+print(type(boolean))
+
+#დავალება 12
+
+number =int(input("choose any number"))
+number1 =int(input("Choose second number"))
+number2 =int(input("choose thrid number"))
+number3 =int(input("Choose forth number"))
+
+print(number + number1 + number2 + number3)
