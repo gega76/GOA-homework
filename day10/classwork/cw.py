381fc8120841bc9bcac9a438c24716cab9322016
gega76 <ggabunia571@gmail.com>
2025-08-03 02:32:35 -0700

    1

---
+++ day10/classwork/cw.py
@@ -0,0 +1,8 @@
+for i in range(50,100,):
+    print(i)
+
+
+for i in range(3,76):
+    print(i)
+
+
---
+++ day10/homework/hw.py
@@ -0,0 +1,68 @@
+#დავალება 1
+for i in range(0,39):
+    print(i)
+
+
+
+#დავალება 2
+for i in range(30,40):
+    print(i)
+
+
+#დავალება 3
+for i in range(12,120):
+    print(i)
+
+
+#დავალება 4
+
+for i in range(18):
+    print("Gega", 13)
+
+
+#დავალება 5
+
+for i in range(10,100,3):
+    print(i)
+
+
+#დავალება 6
+
+for i in range(30):
+    print(i, "banani")
+
+#დავალება 7
+
+for i in range(13):
+    age = input("whas is your age")
+
+
+
+#დავალება 8
+
+i = 0
+
+while i < 10:
+    print("gega")
+    i=i + 1
+
+#დავალება 9
+
+i=0
+
+while i<24:
+    print(i)
+    i = i + 1
+
+
+#დავალება 10
+
+i=0
+
+while i < 20:
+    print("mersedes")
+    i = i + 1
+
+
+for i in range(100000000):
+    print("bmw")
