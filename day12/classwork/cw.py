23ca2dc734670f97bf1bb439a0044e117cf71dab
gega76 <ggabunia571@gmail.com>
2025-08-11 12:13:47 -0700

    1

---
+++ day12/classwork/cw.py
@@ -0,0 +1,25 @@
+# დავალება 1
+for i in range(11):
+    print(i)
+
+
+#დავალება 2
+
+i = 5
+while i < 20:
+    print(i)
+    i = i + 1
+
+#დავალება 3
+
+# conditional statment  — ეს  ასრულებს სხვადასხვა მოქმედებას ანუ if/else იმის მიხედვით თუ რომელი პირობა არის (True) ან (False).
+ 
+
+
+# დავალება 4
+
+age = int(input("what is your age"))
+
+if age > 18:
+    print("you are adult")
+else:  print ("you are teenager")
---
+++ day12/homework/hw.py
@@ -0,0 +1,44 @@
+# დავალება 1
+
+# conditional statment  — ეს  ასრულებს სხვადასხვა მოქმედებას ანუ if/else იმის მიხედვით თუ რომელი პირობა არის (True) ან (False).
+
+
+# დავალება 2
+
+for i in range(50):
+    print("hello world")
+
+
+
+
+# დავალება 3
+
+i = 3
+
+while i < 18:
+    print(i)
+
+    i = i + 1
+
+
+
+# დავალება 4
+
+password = int(input("what is the password"))
+
+if password == 1234:
+    print("password is correct")
+else:
+    print("password is incorrect")
+
+
+
+# დავალება 5 
+
+animal = int(input("choose any animal"))
+
+if animal == "ძაღლი":
+    print("woaf!  woaf! ")
+else:
+    print("შენ არ გყავს ძაღლი")
+
