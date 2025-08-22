318ff805bc06408b8482ee60c4cca9b2aeba26d6
gega76 <ggabunia571@gmail.com>
2025-08-22 04:26:59 -0700

    1

---
+++ day15/homework/hw.py
@@ -0,0 +1,57 @@
+#დავალება  1
+h = 15
+
+if h == 10:
+    print("more than ten")
+
+else:
+    print("less than ten")
+
+
+#დავალება 2
+
+
+i =int(input("choose any number"))
+
+
+if i == 15:
+    print("equat to 15")
+
+else:
+    print("not equal to 15")
+
+
+#დავალება 3
+
+
+f =str(input("Choose a name"))
+
+
+if f == "giorgi":
+    print("you are correct")
+
+else:
+    print("you are wrong")
+
+
+#დავალება 4
+
+for i in range(50,100,5):
+    print(i)
+
+#დავალება 5
+
+
+for i in range(1):
+    print("gega gabunia")
+    
+
+#დავალება 6
+
+
+i = 20
+
+while i < 50:
+    print(i)
+
+    i = i + 1
