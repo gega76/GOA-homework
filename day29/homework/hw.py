0423363c5ff6939c75d84ad3324070b1070b80ab
gega76 <ggabunia571@gmail.com>
2025-10-11 01:05:05 -0700

    1

---
+++ day29/homework/hw.py
@@ -0,0 +1,68 @@
+# დავალება 1
+
+
+
+
+python = "PythonProgramming"
+
+
+print(python[0:6])
+
+
+
+# დავალება 2
+
+
+numbers = [10, 20, 30, 40, 50, 60, 70]
+
+
+print(numbers[-5:-2])
+
+
+
+# დავალება 3
+
+
+
+string = "HelloWorld"
+
+print(string[-10:-5])
+
+
+
+# დავალება 4
+
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+
+print(letters[-7])
+
+print(letters[-5])
+
+print(letters[-3])
+
+
+
+
+# დავალება 5
+
+
+string2 = "Information"
+
+
+print(string2[-9:-4])
+
+
+# დავალება 6
+
+
+string3 = "abcdefghijklmno"
+
+
+print(string3[0:3])
+
+print(string3[-6:-1])
+
+print(string3[5:9])
