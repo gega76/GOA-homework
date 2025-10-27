a67a231eb0a1eda8c16c61a2d043c22a806615bd
gega76 <ggabunia571@gmail.com>
2025-10-27 03:24:00 -0700

    1

---
+++ day34/classwork/cw.py
@@ -0,0 +1,36 @@
+# # დავალება 1
+
+
+
+
+
+# momxmarebeli = (input("choose any word: "))
+
+
+# print(len(momxmarebeli))
+
+
+# for i in range(len[momxmarebeli]):
+#     print(momxmarebeli[i])
+
+
+
+
+
+# დავალება 2
+
+
+
+
+names = ["gega","giorgi","dachi","rezi","lasha"]
+
+
+
+for i in range(len(names)):
+    print(len(i), "== სიმბოლო")
+
+
+    if len(i) % 2 == 0:
+        print("ლუწი")
+    else:
+        print("კენტი")
