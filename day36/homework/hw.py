ccb7646554f460c6ea0de78b7ff8ccc27f98db50
gega76 <ggabunia571@gmail.com>
2025-11-03 06:21:12 -0800

    1

---
+++ day36/homework/hw.py
@@ -0,0 +1,83 @@
+# დავალება 1
+
+
+
+momxmarebeli = input("choose any word: ")
+
+full = 0
+
+for i in range(len(momxmarebeli)):
+    if momxmarebeli[i] == "E" or momxmarebeli[i] == "e":
+        full = full + 1
+        break
+    print("result", momxmarebeli[full])
+
+
+
+
+#  დავალება 2
+
+
+
+winadadeba = input("pick a any sentence: ")
+
+
+
+if "bad" in winadadeba:
+    print("ყველაფერი რიგზეა")
+else:
+    print("აკრძალული სიტყვა")
+
+
+
+
+# დავალება 3
+
+
+
+winadadeba2 = input("choose amy sentence: ")
+
+for i in range(len(winadadeba2)):
+    if winadadeba2[i] != " ":
+        print(winadadeba2[i])
+
+# დავალება 4
+
+
+
+winadadeba3 = input("choose amy sentence: ")
+
+for i in range(len(winadadeba3)):
+    if winadadeba3[i] == "a" or winadadeba3[i] == "e" or winadadeba3[i] == "i" or winadadeba3[i] == "u" or winadadeba3[i] == "o":
+        break
+    else:
+        print(winadadeba3[i])
+
+
+# დავალება 5
+
+
+
+# number1 = int(input("choose any number"))
+# number2 = int(input("choose second number"))
+
+
+# for i in range(()) ვერ გავიგე
+
+
+
+# დავალება 6
+
+password = "python is best"
+
+guest = input("choose any sentence: ")
+
+while guest != password:
+    print("you should learn python")
+    guest = input("choose any sentence: ")
+    
+
+
+# დავალება 7
+
+# ვერ გავიგე
