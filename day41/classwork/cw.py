dc931dd106028e93719fc07ebc7c8b12d2541722
gega76 <ggabunia571@gmail.com>
2025-11-18 08:52:47 -0800

    1

---
+++ day41/classwork/cw.py
@@ -0,0 +1,42 @@
+# დავალება 1
+
+random = [980 , "saba", 231 , "kote" , "cico" , True , "gio" , 40.5]
+
+
+random.append("ianvari")
+
+print(random)
+
+
+
+# დავალება 2
+
+
+random = [980 , "saba", 231 , "kote" , "cico" , True , "gio" , 40.5]
+
+
+random.insert(2)
+
+print(random)
+
+
+
+# დავალება 3
+
+
+random = [980 , "saba", 231 , "kote" , "cico" , True , "gio" , 40.5]
+
+random.pop(5)
+
+print(random)
+
+
+# დავალება 4
+
+random = [980 , "saba", 231 , "kote" , "cico" , True , "gio" , 40.5]
+
+
+random.remove("True")
+random.remove("kote")
+
+print(random)
