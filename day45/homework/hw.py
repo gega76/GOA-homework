cd3d38bb91096d088836cd3be7c1581afcf23d2b
gega76 <ggabunia571@gmail.com>
2025-12-06 09:38:01 -0800

    1

---
+++ day45/homework/hw.py
@@ -0,0 +1,70 @@
+#  დავალება 1
+
+
+# 1)
+
+numbers = [90,91,32,53,86,92]
+
+knew = []
+
+for i in numbers:
+    if i % 2 == 0:
+        knew.append(i)
+
+print(knew)
+
+
+# 2) 
+
+numbers2 = [90,91,32,53,86,92]
+
+knew = []
+
+for i in numbers2:
+    if i % 2 == 1:
+        knew.append(i)
+
+print(knew)
+
+
+
+
+
+# 3)
+
+names = ["giorgi","rezi","aleqsandre",""]
+
+for i in range(len(names) - 1, -1, -1):
+    if names[i][0] == "გ" and names[i][-1] == "ი":
+        names.remove(names[i])
+
+print(names)
+
+
+names2 = ["გეგი", "გიორგი", "ნიკა", "რეზი", "დაჩი", "საბა"]
+
+for i in range(len(names2) - 1, -1, -1):
+    if names2[i][0] == "გ" and names2[i][-1] == "ი":
+        names2.pop(i)
+print(names2)
+
+
+
+# 4)
+
+name = ["Rezi", "Gega", "Dachi", "Saba", "Alexandre"]
+
+for i in range(len(name) - 1, -1, -1):
+    if name[i][0].upper() and i % 2 == 1:
+        name[i].lower()
+    elif name[i][0].upper() and i % 2 == 0:
+        name.pop(i)
+print(name)
+
+# 5) 
+
+name = ["Rezi", "Gega", "Dachi", "Saba", "Alexandre"]
+
+for i in name:
+    if name[i][0].upper() == "G" and name[i][-1] == i.upper():
+        print
