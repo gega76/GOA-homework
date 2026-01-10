0b59ff6591586279abc7ded509f50307d9f989a1
gega76 <ggabunia571@gmail.com>
2026-01-10 00:24:45 -0800

    1

---
+++ day52/homework/hw.py
@@ -0,0 +1,87 @@
+# დავალება 1
+
+#?????
+
+
+# # დავალება 2
+
+
+# text = "PyThOn"
+# result = []
+
+# i = 0
+# while i < len(text):
+#     if text[i] == text[i].lower():
+#         result.append(text[i].upper())
+#     else:
+#         result.append(text[i].lower())
+# i += 1
+
+# print(result)
+
+# # დავალება 3
+
+
+# names = ["gega","dachi","giorgi","rezi","saba","luka","olegi"]
+# result = []
+
+# for i in names:
+#     if i.lower() == i:
+#         result.insert(0, i.upper())
+#     elif i.upper() == i:
+#         result.append(i.lower())
+
+# print(result)
+
+# # დავალება 4
+
+# cities = ["qutaisi","tbilisi","ZUGDIDI","telavi","FOTI","batumi"]
+
+# i = 0
+
+# while i < len(cities):
+#     if cities[i].upper() == cities[i]:
+#         cities.pop(cities[i].upper)
+
+# დავალება 5
+
+
+#????
+
+
+# დავალება 6
+
+text = "PyThOn"
+result = []
+
+for i in text:
+    if i == i.lower():
+        result.append("+")
+    else:
+        result.append("-")
+# მერე ვეღარ გავიგე
+
+
+# დავალება 7
+
+words = "i am from georgia"
+
+
+empty = []
+
+i = 0
+while i < len(words):
+    word = ""
+
+# მერე ვეღარ გავიგე რა გამეკეთბინა
+
+# დავალება 8
+
+
+text = "Python"
+reversed_text = ""
+
+for i in range(len(text)-1, -1, -1):
+    reversed_text += text[i].upper()
+
+print(reversed_text)
