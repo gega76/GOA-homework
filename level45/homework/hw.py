d735f66dd4391dd9ced1574edd05dcdb7b51c602
gega76 <ggabunia571@gmail.com>
2026-03-05 11:55:08 -0800

    1

---
+++ level45/homework/hw.py
@@ -0,0 +1,40 @@
+#codewars 1
+
+
+
+
+#codewars 2
+
+
+def litres(time):
+    return int(time * 0.5)
+
+
+#codewars 3
+
+
+
+
+#codewars 4
+
+def lovefunc( flower1, flower2 ):
+    if flower1 % 2 == 0 and flower2 % 2 == 1:
+        return True
+    if flower2 % 2 == 0 and flower1 % 2 == 1:
+        return True
+    else:
+        return False
+    
+#codewars 5
+
+
+def maps(a):
+    result = []
+    for i in a:
+        result.append(i * 2)
+    return result
+
+
+
+#codewars 6
+
