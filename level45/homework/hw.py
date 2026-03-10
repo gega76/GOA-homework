87a407e3d3e5c5f97893410843224877f7cfb90c
gega76 <ggabunia571@gmail.com>
2026-03-10 06:20:18 -0700

    1

--- level45/homework/hw.py
+++ level45/homework/hw.py
@@ -12,7 +12,16 @@ def litres(time):
 
 #codewars 3
 
+def to_jaden_case(string):
+    words = string.split()
+    result = ""
 
+    for i in range(len(words)):
+        result = result + words[i].capitalize()
+        if i != len(words) - 1:
+            result = result + " "
+
+    return result
 
 
 #codewars 4
@@ -38,3 +47,17 @@ def maps(a):
 
 #codewars 6
 
+def solution(text, ending):
+    if len(ending) > len(text):
+        return False
+
+    empty = ""
+    i = len(text) - len(ending)
+    while i < len(text):
+        empty = empty + text[i]
+        i = i + 1
+
+    if empty == ending:
+        return True
+    else:
+        return False
