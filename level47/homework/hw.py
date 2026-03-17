37887e899dd8f7700a8969490f5ea977faefd742
gega76 <ggabunia571@gmail.com>
2026-03-17 10:22:19 -0700

    1

--- level47/homework/hw.py
+++ level47/homework/hw.py
@@ -57,7 +57,8 @@ def check_exam(arr1, arr2):
 
 # codewars 5
 
-# ვერ გავიგე
+def create_phone_number(n):
+    return '(' + str(n[0]) + str(n[1]) + str(n[2]) + ') ' + str(n[3]) + str(n[4]) + str(n[5]) + '-' + str(n[6]) + str(n[7]) + str(n[8]) + str(n[9])
 
 # codewars 6
 
