02e805e470dcaf99b2045927a783d51f961fc477
gega76 <ggabunia571@gmail.com>
2026-05-07 10:42:25 -0700

    1

--- level60/homework/hw.py
+++ level60/homework/hw.py
@@ -15,7 +15,11 @@ def compute_sum(n):
 
 #codewars 3
 
-
+def is_valid_walk(walk):
+    if len(walk) == 10 and walk.count("n") == walk.count("s") and walk.count("w") == walk.count("e"):
+        return True
+    else:
+        return False
 
 
 #codewars 4
