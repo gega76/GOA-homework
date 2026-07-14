d3657c56f883d9d01799b82ea653be10940f765c
gega76 <ggabunia571@gmail.com>
2026-07-14 11:58:13 -0700

    1

---
+++ level81/classwork/cw.py
@@ -0,0 +1,12 @@
+# დავალება 1
+
+# def calc_type(a, b, res) -> str:
+#     if a + b == res:
+#         return "addition"
+#     elif a - b == res:
+#         return "subtraction"
+#     elif a * b == res:
+#         return "multiplication"
+#     else:
+#         return "division"
+    
