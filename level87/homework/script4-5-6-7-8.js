050aab7d7b9f93e0667054b85782fc8236f976f2
gega76 <ggabunia571@gmail.com>
2026-08-04 22:51:31 -0700

    1

---
+++ level87/homework/script4-5-6-7-8.js
@@ -0,0 +1,18 @@
+//  console.log("gega")
+
+//  console.log(13)
+
+/*  console.log(1.71)
+
+string -> რაც ბრჭყალებშია
+
+number -> ნებისმისერი ათწილადი და მთელი რიცხვი
+
+boolean -> true და false
+
+Bigint -> დიდი რიცხვი
+
+null -> მნიშვნელობა არარსებობს
+
+undefined -> არ არის მნიშვნელობა შექმნილი
+*/
