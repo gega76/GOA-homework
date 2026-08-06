a986705d89e9ba8cbdd6f70ca1893131d7c5ea4e
gega76 <ggabunia571@gmail.com>
2026-08-06 08:50:08 -0700

    1

---
+++ level88/classwork/cw.js
@@ -0,0 +1,60 @@
+// დავალება 1
+
+console.log(10 % 2)
+
+console.log(10 * 2)
+
+console.log(10 / 2)
+
+console.log(10 + 2)
+
+console.log(10 - 2)
+
+
+// დავალება 2
+
+console.log("gega" + "gabunia")
+
+
+// დავალება 3
+
+
+console.log("15" - 5)
+
+console.log("15" % 5)
+
+console.log("15" / 5)
+
+
+
+
+
+// დავალება 4
+
+
+console.log("gega".length)
+
+
+
+// დავალება 5
+
+
+console.log("gega".toUpperCase())
+
+console.log("gega".toLowerCase())
+
+//დავალება 6
+
+
+console.log("gega".startsWith("g"))
+
+
+//დავალება 7
+
+console.log("       gega       ".trim)
+
+
+// დავალება 8
+
+
+console.log("ჰიდროელექტროსადგური".slice(6 , 12))
---
+++ level88/homework/hw.js
@@ -0,0 +1,80 @@
+// დავალება 1
+
+console.log(10 % 2)
+
+console.log(10 * 2)
+
+console.log(10 / 2)
+
+console.log(10 + 2)
+
+console.log(10 - 2)
+
+
+// დავალება 2
+
+console.log("Hello " + "World")
+
+
+// დავალება 3
+
+
+console.log("My" + "name" + "is" + "Nika")
+
+
+// დავალება 4
+
+console.log("I" + "live" + "in" + "Tbilisi")
+
+
+// დავალება 5
+
+console.log("JavaScript".length)
+
+
+// დავალება 6
+
+console.log("javascript".toUpperCase())
+
+// დავალება 7
+
+console.log("JAVASCRIPT".toLowerCase())
+
+// დავალება 8
+
+console.log("      Hello      ".trim)
+
+
+// დავალება 9
+
+console.log("Programming".startsWith("P"))
+
+// დავალება 10
+
+console.log("Programming".slice(0 , 4))
+
+// დავალება 11
+
+console.log("JavaScript".slice(7))
+
+// დავალება 12
+
+console.log("      JavaScript      ".trim)
+console.log("      JavaScript      ".length)
+console.log("      JavaScript      ".toUpperCase)
+
+
+// დავალება 13
+
+console.log("Programming".slice(0 , 7))
+console.log("Programming".toLowerCase())
+console.log("Programming".length)
+
+// დავალება 14
+
+console.log("      JavaScript      ".trim)
+console.log("      JavaScript      ".startsWith("Java"))
+console.log("      JavaScript      ".slice(0 , 4))
+console.log("      JavaScript      ".toUpperCase())
+console.log("      JavaScript      ".length)
+console.log("Language:" + "JAVA")
