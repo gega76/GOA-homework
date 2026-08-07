4b2ab955002d5a840ff20f6fabd47574e346e51f
gega76 <ggabunia571@gmail.com>
2026-08-07 02:52:48 -0700

    1

---
+++ level89/classowork/cw.js
@@ -0,0 +1,41 @@
+// დავალება 1
+
+console.log("gega".repeat(15))
+
+// დავალება 2
+
+console.log("i am going to dubai".replace("dubai" , "tbilisi"))
+
+
+// დავალება 3
+
+
+console.log("my friend told me to tell my other friend to come".replaceAll("friend" , "bro"))
+
+
+// დავალება 4
+
+let name
+
+console.log(name)
+
+name = "gega"
+
+console.log(name)
+
+
+
+// დავალება 5
+
+
+const number
+
+console.log(number)
+
+number = 15
+
+console.log(number)
+
+number = "gega"
+
+console.log(number)
