d837c38b4601369c9dc306c26685eea7618d2afa
gega76 <ggabunia571@gmail.com>
2026-08-08 00:29:06 -0700

    1

---
+++ level89/homework/hw.js
@@ -0,0 +1,133 @@
+// დავალება 1
+
+
+// let text = "I like cats";
+
+// console.log(text.replace("cats" , "dogs"))
+
+
+// დავალება 2
+
+
+// let text = "Hello Goga";
+
+// console.log(text.replace("Goga" , "Nika"))
+
+
+// დავალება 3
+
+
+// let text = "apple banana apple orange apple";
+
+// console.log(text.replaceAll("apple" , "mango"))
+
+
+// დავალება 4
+
+// let text = "JavaScript is easy. JavaScript is fun.";
+
+// console.log(text.replaceAll("JavaScript" , "Python"))
+
+
+// დავალება 5
+
+// let text = "JavaScript";
+
+// console.log(text.slice(0 , 4))
+
+
+// დავალება 6
+
+// let text = "Programming";
+
+// console.log(text.slice(3 , 7))
+
+
+// დავალება 7
+
+// let score = 50;
+
+// score = 100
+
+// console.log(score)
+
+
+// დავალება 8
+
+// let number = 10;
+
+// number += 5
+
+// console.log(number)
+
+// დავალება 9
+
+// let money = 100;
+
+// money -= 30
+
+// console.log(money)
+
+
+// დავალება 10
+
+// let number = 5;
+
+// number *= 4
+
+// console.log(number)
+
+
+// დავალება 11
+
+// let number = 100;
+
+// number /= 5
+
+// console.log(number)
+
+
+// დავალება 12
+
+// let balance = 500;
+
+// balance -= 100
+// balance += 200
+// balance -= 50
+// balance *= 2
+
+// console.log(balance)
+
+
+// დავალება 13
+
+// let number = 25;
+
+// number --
+
+// console.log(number)
+
+// number ++ 
+
+// console.log(number)
+
+
+// დავალება 14
+
+// let age = 15;
+// const country = "Georgia";
+
+// age = 30
+
+// country = "USA" --> არ შეგვიძლია შეცვლა
+
+// დავალება 15
+
+// let price = 100;
+// let quantity = 3;
+
+// console.log(price + quantity)
+
+// price -= 20
+
+// console.log(price + quantity)
