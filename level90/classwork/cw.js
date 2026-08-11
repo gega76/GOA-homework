664f7f65e323c8d54f784f896a2cc6a7bfb3bf8d
gega76 <ggabunia571@gmail.com>
2026-08-11 01:50:21 -0700

    1

---
+++ level90/classwork/cw.js
@@ -0,0 +1,36 @@
+// დავალება 1
+
+let name = "gega"
+
+console.log(typeof name)
+
+name = true
+
+console.log(typeof name)
+
+name = 15
+
+console.log(typeof name)
+
+// დავალება 2
+
+let age = 13
+let name2 = "gega"
+
+console.log(`my name is ${name2} and my age is ${age}`)
+
+console.log("my name is " + name2 + " and my age is " + age)
+
+
+
+// დავალება 3
+
+
+let name3 = "gega"
+
+
+if(name3 == "saba"){
+    console.log("this variable is holding saba")
+}else{
+    console.log("this variable is holding other name")
+}
