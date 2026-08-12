f2359479dcbb328cd3da19ea5f76fc99a899156b
gega76 <ggabunia571@gmail.com>
2026-08-12 02:18:28 -0700

    q

---
+++ level90/homework/hw.js
@@ -0,0 +1,96 @@
+// დავალება 1
+
+let name = "gega"
+
+let surname = "gabunia"
+
+let age = 13
+
+let adress = "Tbilisi"
+
+console.log(`hello,my name is ${name},my surname is ${surname} and my age is ${age} , i live in ${adress}`)
+
+
+// დავალება 2
+
+let name2 = "gega"
+
+let age2 = 15
+
+let some = true
+
+let nothing
+
+console.log(typeof name2)
+
+console.log(typeof age2)
+
+console.log(typeof some)
+
+console.log(typeof nothing)
+
+// დავალება 3
+
+
+let name3 = "Gega"
+
+if(name3.startsWith("G")){
+    console.log("my name starts with G")
+}else{
+    console.log("my name doesn't start with G")
+}
+
+// დავალება 4
+
+let word = "javascript"
+
+if(word.startsWith("java")){
+    console.log("This word starts with java")
+}else{
+    console.log("This word doesn't start with java")
+}
+
+// დავალება 5
+
+let username = "xinkala"
+
+if(username.startsWith("admin")){
+    console.log("Welcome, Admin!")
+}else{
+    console.log("Welcome, User!")
+}
+
+// დავალება 6
+
+
+let name4 = "Goga"
+
+let city = "Tbilisi"
+
+console.log(`My name is ${name4} and I live in ${city}.`)
+
+
+// დავალება 7
+
+let username2 = "adminTheboss"
+
+let rname = "rezi"
+
+if(username2.startsWith("admin")){
+    console.log(`Welcome, ${rname} ! You are an admin.`)
+}else{
+    console.log(`Welcome, ${rname}! You are a regular user.`)
+}
+
+
+// დავალება 8
+
+let name5 = "gega"
+
+let username3 = "student"
+
+if(username3.startsWith("teacher")){
+    console.log(`Hello ${name5}, you are a teacher.`)
+}else{
+    console.log(`Hello ${name5}, you are a student.`)
+}
