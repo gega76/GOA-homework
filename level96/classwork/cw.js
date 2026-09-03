07de51ab5dfb8e74c631b9aa2e887924003eda48
gega76 <ggabunia571@gmail.com>
2026-09-03 02:48:58 -0700

    1

---
+++ level96/classwork/cw.js
@@ -0,0 +1,28 @@
+// დავალება 1
+
+
+const list1 = [20 , true , "Gega" , 2.19 , "Tbilisi"]
+
+console.log(list1[0])
+console.log(list1[1])
+console.log(list1[2])
+console.log(list1[3])
+console.log(list1[4])
+
+
+// დავალება 2
+
+
+let scores = [45, 78, 32, 90, 56, 67, 23, 84, 39, 71, 60, 95];
+
+scores[0] = scores[0] + 15
+scores[2] = 50
+scores[4] = scores[4] + 20
+scores[6] = scores[6] * 2
+scores[8] = scores[8] - 10
+scores[11] = 100
+scores[3] = 75
+scores[1] = scores[1] + scores[8]
+
+
+console.log(scores)
