570509bdac27390cff6e3d071be6b08e84c72239
gega76 <ggabunia571@gmail.com>
2026-09-05 07:15:16 -0700

    1

---
+++ level96/homework/hw.js
@@ -0,0 +1,98 @@
+// დავალება 1
+
+
+// let numbers = [12, 45, 7, 23, 89, 34, 16, 50];
+// let change = numbers[3]
+// numbers[0] = 100
+// numbers[7] = 200
+// numbers[2] = numbers[2] + 10
+// numbers[4] = numbers[4] / 2
+// numbers[3] = numbers[6]
+// numbers[6] = change
+
+// console.log(numbers)
+
+
+// დავალება 2
+
+
+// let numbers = [15, 8, 42, 8, 31, 42, 19, 8];
+
+// numbers[1] = 80
+// numbers[2] = 420
+// numbers[7] = 800
+// numbers[1] = numbers[1] + 5
+
+
+// დავალება 3
+
+
+// let fruits = ["apple", "banana", "orange", "kiwi", "mango", "peach"];
+
+// let index = 3;
+
+// console.log(fruits[index])
+
+// fruits[index] = "Watermelon"
+
+// console.log(fruits[index])
+
+// index = 5
+
+// console.log(fruits[index])
+
+
+
+// დავალება 4
+
+
+// let names = ["gega" , "rezi", "dachi", "alexsi", "giorgi", "saba"]
+
+// let number = "4"
+
+// number = Number(number)
+
+// console.log(names[number])
+
+
+// დავალება 5
+
+
+// let students = ["Giorgi", "Nika", "Saba", "Luka", "Dato", "Ana"];
+
+// let position = 4;
+// let newName = "Goga";
+
+// if(position <= students.length){
+//     students[position - 1] = newName
+// }else{
+//     console.log("Invalid position")
+// }
+// console.log(students)
+
+// დავალება 6
+
+// let colors = ["red", "blue", "green", "yellow", "black", "white"];
+
+// colors[2] = "purple"
+// colors[4] = "pink"
+// colors[6] = "red"
+// colors = colors.slice(1)
+// console.log(colors)
+
+
+// დავალება 7
+
+// let numbers = [5, 10, 15, 20, 25, 30, 35, 40];
+
+// numbers[1] = numbers[1] * 10
+// numbers[2] = numbers[2] * 10
+// numbers[3] = numbers[3] * 10
+// numbers[4] = numbers[4] * 10
+// numbers[5] = numbers[5] * 10
+
+// დავალება 8
+
+// let numbers = [10, 20, 30, 40, 50, 60, 70, 80];
+
+// ვერ  გავიგე
