8ffcade92c979a238fcf1fe976f9ff81566a7adc
gega76 <ggabunia571@gmail.com>
2026-09-10 02:55:13 -0700

    1

---
+++ level98/classwork/cw.js
@@ -0,0 +1,10 @@
+// დავალება 1
+
+const school = [["Math", ["Giorgi", "Nika", "Saba"]],["English", ["Ana", "Mariam", "Luka"]],["Programming", [["JavaScript", ["Dato", "Giga", "Tato"]],["Python", ["Sandro", "Nino", "Gio"]]]]];
+
+console.log(school[2][1][0][1][1])
+console.log(school[2][1][1][1][1])
+console.log(school[2][1][0][1][1])
+console.log(school[2][1][1][1][0])
+console.log(school[1][1][1])
+console.log(school[2][1][0][1][2])
