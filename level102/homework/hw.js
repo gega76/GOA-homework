7fda24c591847d922a4d6452e9aac80fdc00d42e
gega76 <ggabunia571@gmail.com>
2026-09-26 06:13:52 -0700

    1

---
+++ level102/homework/hw.js
@@ -0,0 +1,73 @@
+// დავალება 1
+
+
+// function processNumber(number , operation){
+//     return operation(number)
+// }
+
+// function double(number){
+//     return number * 2
+// }
+// function triple(number){
+//     return number * 3
+// }   
+// function square(number){
+//     return number * number
+// }
+
+// console.log(processNumber(5, double))
+// console.log(processNumber(5, triple))
+// console.log(processNumber(5, square))
+
+
+// დავალება 2
+
+
+// function processText(text, action){
+//     return action(text)
+// }
+
+// function makeUpperCase(text){
+//     return text.toUpperCase()
+// }
+
+// function makeLowerCase(text){
+//     return text.toLowerCase()
+// }
+
+// function getLength(text){
+//     return text.length
+// }
+
+// console.log(processText("JavaScript", makeUpperCase))
+// console.log(processText("JavaScript", makeLowerCase))
+// console.log(processText("JavaScript", getLength))
+
+
+// დავალება 3
+
+
+// function calculate(a, b, operation){
+//     return operation(a , b)
+// }
+
+// function add(a, b){
+//     return a + b
+// }
+// function subtract(a, b){
+//     return a - b
+// }
+// function multiply(a, b){
+//     return a * b
+// }
+// function divide(a, b){
+//     return a / b
+// }
+
+
+// console.log(calculate(10, 5, add))
+// console.log(calculate(10, 5, subtract))
+// console.log(calculate(10, 5, multiply))
+// console.log(calculate(10, 5, divide))
+
+
